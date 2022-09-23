f0dcb92cda1b06d708e87c67bb5891512d461244
faltiska <alfred.faltiska@outlook.com>
2022-09-23 20:42:56 +0300

    Revert "@Voice 19.4.3"
    This reverts commit f4a250e9763b876c1691d7f968eb8faaf937ffcd.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,6 @@
 # EbookConv project
 NDK_TOOLCHAIN_VERSION := clang
-APP_PLATFORM := android-21
+APP_PLATFORM := android-16
 APP_CFLAGS += -Wno-write-strings -Wno-logical-op-parentheses -Wno-unsequenced -Wno-parentheses -Wno-switch -Wno-#warnings -Wno-invalid-source-encoding
 APP_CPPFLAGS += -frtti -fexceptions -Wno-logical-op-parentheses -Wno-unsequenced -Wno-parentheses -Wno-switch -Wno-#warnings -Wno-invalid-source-encoding
 APP_ARM_MODE := thumb
